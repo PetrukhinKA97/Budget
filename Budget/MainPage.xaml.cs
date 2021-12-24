b449d1c5a6df23aae9f169267a199f752ffcf88e
PetrukhinKA97 <PetrukhinKA97@yandex.ru>
2021-12-25 02:38:22 +0300

    Добавьте файлы проекта.

---
+++ Budget/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Budget
+{
+    public partial class MainPage : Shell
+    {
+        int count = 0;
+     
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void Button_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times.";
+        }
+
+    }
+}
